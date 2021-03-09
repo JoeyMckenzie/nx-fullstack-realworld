d5003e698fa74f39124181e6b8a933758c39ddab
Joey Mckenzie <joey.mckenzie27@gmail.com>
2021-03-08 18:13:30 -0800

    initial commit

---
+++ apps/conduit/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@nx-fullstack-realworld/api-interfaces';
+
+@Component({
+  selector: 'nx-fullstack-realworld-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
