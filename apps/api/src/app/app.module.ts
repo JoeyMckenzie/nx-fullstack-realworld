c818d6c99edce07afcdc51ba09c1208bffa3feeb
Joey Mckenzie <joey.mckenzie27@gmail.com>
2021-04-20 11:37:09 -0700

    chore(Api): Remove unusued services and controllers

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 import { BackendUsersModule } from '@nx-fullstack-realworld/backend/users';
 
 @Module({
   imports: [BackendUsersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
